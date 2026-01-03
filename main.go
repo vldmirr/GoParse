71fd2790375ff8ef6b891e3bd7790312a3e388d6
vldmirr <tsutsura18@gmail.com>
2026-01-03 23:04:00 +0300

    changes struct news

--- main.go
+++ main.go
@@ -1,40 +1,45 @@
 package main
 
-import(
-	// "net/http"
+import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
-	// "time"
 )
 
-type news struct{
+type news struct {
 	name string
 	link string
 }
 
-
-
-func main(){
+func main() {
 	var s []string
-	// var result []news
+	var results []news
 
 	for i := 1; i < 10; i++ {
-		s=append(s,fmt.Sprintf("https://news.ycombinator.com/?p=%d", i))
+		s = append(s, fmt.Sprintf("https://news.ycombinator.com/?p=%d", i))
 	}
-	
+
 	c := colly.NewCollector()
 
-	for _,value:=range s{
-		c.OnHTML(".title > span > a", func(e *colly.HTMLElement) {
-			e.Request.Visit(e.Attr("href"))
-		})	
+	c.OnHTML(".titleline > a", func(e *colly.HTMLElement) {
 
-		c.OnRequest(func(r *colly.Request) {
-			fmt.Println("Visiting",r.URL)
+		title := e.Text
+		link := e.Attr("href")
+		
+		results = append(results, news{
+			name: title,
+			link: link,
 		})
+		
+		fmt.Printf("Title: %s\nLink: %s\n\n", title, link)
+	})
 
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visit: %s\n", r.URL)
+	})
+
+	for _, value := range s {
 		c.Visit(value)
 	}
-	
 
+	fmt.Printf("%d\n", len(results))
 }
